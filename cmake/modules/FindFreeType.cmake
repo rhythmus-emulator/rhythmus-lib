d547b0446b2cbe6b2ff69fd4fe0d4c0c26c1bf0d
kuna <kgdgwn98@gmail.com>
2019-08-05 22:20:35 +0900

    Added freetype

---
+++ cmake/modules/FindFreeType.cmake
@@ -0,0 +1,35 @@
+# Should work well only in MSVC
+
+if (WIN32)
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        # 64bit
+        set(LIB_PATH ${CMAKE_CURRENT_LIST_DIR}/../../prebuilt/x64-MT)
+    else ()
+        # 32bit
+        set(LIB_PATH ${CMAKE_CURRENT_LIST_DIR}/../../prebuilt/x86-MT)
+    endif ()
+else ()
+    set(LIB_PATH ${CMAKE_CURRENT_LIST_DIR}/../../prebuilt/linux-x86)
+endif ()
+
+FIND_PATH(FREETYPE_INCLUDE_DIR freetype/freetype.h
+    HINTS
+    ${CMAKE_CURRENT_LIST_DIR}/../../include
+    PATHS
+    /usr/local/include
+    /usr/include
+)
+
+FIND_LIBRARY(FREETYPE_LIBRARY
+    NAMES libfreetype_static libfreetype freetype
+    HINTS
+    ${LIB_PATH}
+    PATHS
+    /usr/local/lib
+    /usr/lib
+)
+
+SET(FREETYPE_FOUND "NO")
+IF(FREETYPE_LIBRARY AND FREETYPE_INCLUDE_DIR)
+    SET(FREETYPE_FOUND "YES")
+ENDIF(FREETYPE_LIBRARY AND FREETYPE_INCLUDE_DIR)
